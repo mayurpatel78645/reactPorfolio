cba8d09ecd683a40309c63ffcdcfdb7587058e99
mayurpatel78645 <mayurpatel78645@gmail.com>
2021-06-24 19:16:03 -0500

    fix forms

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -5,12 +5,12 @@ export default function Contact() {
 
   const [message, setMessage] = useState(false);
 
-  const handleSubmit = (e) => {
+  /*const handleSubmit = (e) => {
     e.preventDefault();
     setMessage(true);
     e.target[0].value = "";
     e.target[1].value = "";
-  }
+  }*/
 
   return (
     <div className='contact' id='contact'>
@@ -19,12 +19,12 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form name='contact v1' method='POST' data-netlify='true' onSubmit={handleSubmit}>
+        <form name='contact v1' method='POST' data-netlify='true' onSubmit="submit">
           <input type="hidden" name='form-name' value='contact v1' />
           <input name='email' type="email" required placeholder='Email' />
           <textarea name='message' placeholder='Message'></textarea>
           <button type='submit'>Send</button>
-          {message && <span>Thank You! Have a nice day!</span>}
+          {/*message && <span>Thank You! Have a nice day!</span>*/}
         </form>
       </div>
     </div>
